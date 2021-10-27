9bc8278b0930568acb4cb97246decb5c46470fcf
Mridul <gmridul556@gmail.com>
2021-10-27 23:42:08 +0530

    final(2)

--- commands_implemented/organize.js
+++ commands_implemented/organize.js
@@ -5,10 +5,11 @@ let path = require("path");
 
 
 let types = {
-    media: ["mp4", "mkv", "jpeg"],
+    media: ["mp4", "mkv", "jpeg", "jpg"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"]
+    app: ['exe', 'dmg', 'pkg', "deb"],
+    programming_files:["py", "js", "java", "cpp"]
   }
 
 
